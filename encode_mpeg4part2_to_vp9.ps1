a2aad45777932e405538ee95ba81552a722ec32f
Julien Milletre Akipe <code.julien@milletre.fr>
2024-05-26 10:52:12 +0200

    Remove parallel encoding feature

--- encode_mpeg4part2_to_vp9.ps1
+++ encode_mpeg4part2_to_vp9.ps1
@@ -19,13 +19,11 @@ Function Get-WorkspaceFolders([string] $InstructionMessage, [string] $PathSepara
 
     return $ListFolders
 }
-$Fun_Get_WorkspaceFolders = ${function:Get-WorkspaceFolders}.ToString()
 
 Function Get-LogPath()
 {
     return "$ENV:AppData\$APP_NAME.log"
 }
-$Fun_Get_LogPath = ${function:Get-LogPath}.ToString()
 
 Function Write-LogMessage()
 {
@@ -39,20 +37,17 @@ Function Write-LogMessage()
 
         Add-Content -Path $(Get-LogPath) -Value $Content
         Write-Host -ForegroundColor $LOG_INFO_COLOR $Content
-        # Write-Host $Content
     } Catch {
         Write-Host -ForegroundColor $LOG_ERROR_COLOR (
             "Error: " + $_.Exception.Message
         )
     }
 }
-$Fun_Write_LogMessage = ${function:Write-LogMessage}.ToString()
 
 Function Get-FullNameWithoutExtension([io.fileinfo] $File)
 {
     return $File.FullName.Remove($File.FullName.Length - $File.Extension.Length)
 }
-$Fun_Get_FullNameWithoutExtension = ${function:Get-FullNameWithoutExtension}.ToString()
 
 Function Use-EncodeToVP9([io.fileinfo] $File, $FileNumber)
 {
@@ -75,10 +70,9 @@ Function Use-EncodeToVP9([io.fileinfo] $File, $FileNumber)
         ((Get-FullNameWithoutExtension $File) + '.webm') +
         "'"
     
-    Write-LogMessage ("[File $FileNumber] Encoding command : " + $EncodingCommand)
+    Write-LogMessage ("[$FileNumber] Encoding command : " + $EncodingCommand)
     Invoke-Expression $EncodingCommand
 }
-$Fun_Use_EncodeToVP9 = ${function:Use-EncodeToVP9}.ToString()
 
 # App
 
@@ -86,8 +80,6 @@ Function Use-RunApp()
 {
     Write-LogMessage "Starting a new encoding session"
     Write-LogMessage ('Log file location : "' + $(Get-LogPath) + '"')
-    $EncoderParallel = Read-Host -Prompt "How many parallel encodings do you want ?"
-    Write-LogMessage ('Number of parallel encodings : "' + $EncoderParallel + '"')
 
     $Paths = Get-WorkspaceFolders 'List of folders where all encoding for MPEG-4 Part 2 (DivX) videos is performed (seperated by a ;)' ";"
 
@@ -100,31 +92,14 @@ Function Use-RunApp()
         
         $VideosToEncode = Get-ChildItem $CurrentPath -Filter $DIVX_FILE_EXTENSION
         
-        $VideosToEncode | Foreach-Object -ThrottleLimit $EncoderParallel -Parallel {
+        $VideosToEncode | Foreach-Object {
             $File = $_
-
-            $FileNumber = (($using:VideosToEncode).IndexOf($File) + 1);
-            
-            Set-Variable APP_NAME -Option Constant -Value $using:APP_NAME
-            Set-Variable DIVX_FILE_EXTENSION -Option Constant -Value $using:DIVX_FILE_EXTENSION
-            Set-Variable LOG_DATE_FORMAT -Option Constant -Value $using:LOG_DATE_FORMAT
-            Set-Variable LOG_ERROR_COLOR -Option Constant -Value $using:LOG_INFO_COLOR
-            Set-Variable LOG_INFO_COLOR -Option Constant -Value DarkGray
-            Set-Variable ENCODER_VIDEO_MINIMUM_RATE -Option Constant -Value $using:ENCODER_VIDEO_MINIMUM_RATE
-            Set-Variable ENCODER_VIDEO_MAXIMUM_RATE -Option Constant -Value $using:ENCODER_VIDEO_MAXIMUM_RATE
-            Set-Variable ENCODER_VIDEO_AVERAGE_RATE -Option Constant -Value $using:ENCODER_VIDEO_AVERAGE_RATE
-            Set-Variable ENCODER_AUDIO_RATE -Option Constant -Value $using:ENCODER_AUDIO_RATE
-
-            ${function:Get-WorkspaceFolders} = $using:Fun_Get_WorkspaceFolders
-            ${function:Get-LogPath} = $using:Fun_Get_LogPath
-            ${function:Write-LogMessage} = $using:Fun_Write_LogMessage
-            ${function:Get-FullNameWithoutExtension} = $using:Fun_Get_FullNameWithoutExtension
-            ${function:Use-EncodeToVP9} = $using:Fun_Use_EncodeToVP9
+            $FileNumber = ($VideosToEncode.IndexOf($File) + 1);
 
             if ($File -is [io.fileinfo]) {
-                Write-LogMessage ('[File ' + $FileNumber + '] Starting conversion for "' + $File + '" ...')
-                Use-EncodeToVP9 $File $FileNumber
-                Write-LogMessage ('[File ' + $FileNumber + '] Conversion completed for "' + $File + '"')
+                Write-LogMessage ('[' + $FileNumber + '/' + $VideosToEncode.Length + '] Starting conversion for "' + $File + '" ...')
+                Use-EncodeToVP9 $File ("" + $FileNumber + '/' + $VideosToEncode.Length)
+                Write-LogMessage ('[' + $FileNumber + '/' + $VideosToEncode.Length + '] Conversion completed for "' + $File + '"')
             }
         }
 
